11c595756e693484b6060fe2bc80738f0d217d92
Lúcás Meier <cronokirby@gmail.com>
2020-11-20 13:07:24 +0100

    Setup basic CMake build
    This supports nested source directories, which is nice. Otherwise it just builds stuff using C++17

---
+++ src/lib.cpp
@@ -0,0 +1,5 @@
+#include "lib.hpp"
+
+std::string message() {
+  return "Hello World!\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "lib.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << message();
+  return 0;
+}
